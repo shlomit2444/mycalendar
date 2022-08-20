7836d47478a58ad80f5c6e683ff5ff1ec8f41535
shlomit <shlomitn80@gmail.com>
2022-08-20 23:09:32 +0300

    this is the first commit3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const console = require('console');
 const http = require('http');
 const app = require('./app');
 const svr = http.createServer(app);
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 8080
 const server = http.createServer(app);
 //svr.listen(port,()=>{
 //console.log("server is up");
